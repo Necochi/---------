bbd17c49b060048c9254ab29eb1fe734506678c8
Necochi <LikaGirl0@gmail.com>
2024-09-06 22:28:39 +0300

    updated path

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   build: {
-    outDir: './public',
+    outDir: "/public",
     emptyOutDir: false,
   },
   plugins: [react()],
