78a9bf00d36337dcb60af84fb16992f9a5ce52de
Necochi <LikaGirl0@gmail.com>
2024-09-06 22:35:54 +0300

    new paths

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   build: {
-    outDir: path.resolve(__dirname, 'public'),
+    outDir: path.resolve(__dirname, "public"),
     emptyOutDir: false,
   },
   plugins: [react()],
